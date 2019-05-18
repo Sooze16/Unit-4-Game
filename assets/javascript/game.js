7831f879e70c6e79370a0f1cf753e8b3a14b0cdb
Sooze16 <slkpes2002@yahoo.com>
2019-05-18 15:54:35 -0500

    class help with phil

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,81 +1,104 @@
-// $(document).ready(function() {
-
-var snoopysNumber;
-var snowFlakeTotal = 0;
-var wins = 0;
-var losses = 0;
-var snowFlake0Num;
-var snowFlake1Num;
-var snowFlake2Num;
-var snowFlake3Num;
-// var imageArray ["."]
-
-function numbers() {
-  snoopysNumber = Math.floor(Math.random() *101) + 19;
-  snowFlake0Num = Math.ceil(Math.random() *11) + 1;
-  snowFlake1Num = Math.ceil(Math.random() *11) + 1;
-  snowFlake2Num = Math.ceil(Math.random() *11) + 1;
-  snowFlake3Num = Math.ceil(Math.random() *11) + 1;
-  // console.log(snoopysNumber)
-}
-
-function game () {
-  numbers();
-  snowFlakeTotal = 0;
-  $("#snoopysNumber").text(snoopysNumber);
-  $("#snowFlakeTotal").text(snowFlakeTotal);
-  $("#snowFlake0").attr("data-sfValue", snowFlake0);
-  $("#snowFlake1").attr("data-sfValue", snowFlake1);
-  $("#snowFlake2").attr("data-sfValue", snowFlake2);
-  $("#snowFlake3").attr("data-sfValue", snowFlake3);
-  $("#wins").text(wins);
-  $("#losses").text(losses);
-  $("#winOrLose").text("");
-
-  
-
-}
-
-function youWin() {
-  $("#winOrLose").text("YOU WIN!");
-  wins++;
-  $("#wins").text(wins);
-}
-
-function youLose() {
-  $("#winOrLose").text("YOU LOSE");
-  losses++;
-  $("#losses").text(losses);
-}
-
-$(".flakeImg").hover(function() {
-  $(this).css({opacity: 0.3});
-},
-function() {
-  $(this).css({opacity: 1});
-});
-
-newGame();
+$(document).ready(function() {
 
 
-// Function to add the snowflake values together
-$(".flakeImg").on("click", function() {
-  if (snowFlakeTotal >= snoopysNumber) {
-    return;
-  }
+    // var snoopysNumber;
+    var snowFlakeTotal = 0;
+    var wins = 0;
+    var losses = 0;
+    var snowFlake0Num;
+    var snowFlake1Num;
+    var snowFlake2Num;
+    var snowFlake3Num;
+    // var imageArray ["."]
 
-  var sfValue = $(this).attr("data-sfValue");
-  sfValue = parseInt(sfValue);
-  snowFlakeTotal += sfValue;
-  $("#snowFlakeTotal").text(snowFlakeTotal);
+    // function numbers() {
+    //   snoopysNumber = Math.floor(Math.random() *101) + 19;
+    //   snowFlake0Num = Math.ceil(Math.random() *11) + 1;
+    //   snowFlake1Num = Math.ceil(Math.random() *11) + 1;
+    //   snowFlake2Num = Math.ceil(Math.random() *11) + 1;
+    //   snowFlake3Num = Math.ceil(Math.random() *11) + 1;
+    //   // console.log(snoopysNumber)
 
-  if (snowFlakeTotal === snoopysNumber) {
-    youWin();
-  } else if (snowFlakeTotal > snoopysNumber) {
-    youLose();
-  }
-});
 
-$(".btn").on("click", function() {
-  newGame();
+    var snoopysNumber = Math.floor(Math.random() * 101) + 19;
+
+    var snowFlakesNumber = [Math.ceil(Math.random() * 11) + 1, Math.ceil(Math.random() * 11) + 1, Math.ceil(Math.random() * 11) + 1, Math.ceil(Math.random() * 11) + 1];
+
+    var imageArray = ["assets/images/sf0.PNG", "assets/images/sf1.PNG", "assets/images/sf2.PNG", "assets/images/sf3.PNG"];
+
+    for (var i = 0; i < snowFlakesNumber.length; i++) {
+        var snowFlakesPic = $("<img src='" + imageArray[i] + "' data-sfValue= '" + snowFlakesNumber[i] + "' alt='snowflake picture' class='flakeImg' />");
+
+        $("#snowFlakes").append(snowFlakesPic);
+
+    };
+
+
+
+
+
+
+    function newGame() {
+
+        snoopysNumber = Math.floor(Math.random() * 101) + 19;
+
+        snowFlakesNumber = [Math.ceil(Math.random() * 11) + 1, Math.ceil(Math.random() * 11) + 1, Math.ceil(Math.random() * 11) + 1, Math.ceil(Math.random() * 11) + 1];
+
+
+
+
+        snowFlakeTotal = 0;
+        $("#snoopysNumber").text(snoopysNumber);
+        $("#snowFlakeTotal").text("0");
+
+        $("#wins").text(wins);
+        $("#losses").text(losses);
+        //   $("#winOrLose").text("");
+    }
+
+    function youWin() {
+        $("#winOrLose").text("YOU WIN!");
+        wins++;
+        $("#wins").text(wins);
+    }
+
+    function youLose() {
+        $("#winOrLose").text("YOU LOSE");
+        losses++;
+        $("#losses").text(losses);
+    }
+
+    $(".flakeImg").hover(function() {
+            $(this).css({ opacity: 0.3 });
+        },
+        function() {
+            $(this).css({ opacity: 1 });
+        });
+
+    newGame();
+
+
+    // Function to add the snowflake values together
+    $(".flakeImg").on("click", function() {
+        if (snowFlakesNumber >= snoopysNumber) {
+            return;
+        }
+
+        var sfValue = $(this).attr("data-sfValue");
+        sfValue = parseInt(sfValue); //converts number string into numeric value
+        snowFlakeTotal += sfValue;
+        $("#snowFlakeTotal").text(snowFlakeTotal);
+
+        if (snowFlakeTotal === snoopysNumber) {
+            youWin();
+        } else if (snowFlakeTotal > snoopysNumber) {
+            youLose();
+        }
+    });
+
+    $(".btn").on("click", function() {
+        newGame();
+    });
+
+
 });
--- assets/javascript/numbers.js
+++
@@ -1,8 +0,0 @@
-function numbers() {
-  snoopysNumber = Math.floor(Math.random() * 101) + 19;
-  snowFlake0Num = Math.ceil(Math.random() * 11) + 1;
-  snowFlake1Num = Math.ceil(Math.random() * 11) + 1;
-  snowFlake2Num = Math.ceil(Math.random() * 11) + 1;
-  snowFlake3Num = Math.ceil(Math.random() * 11) + 1;
-  console.log(snoopysNumber);
-}
